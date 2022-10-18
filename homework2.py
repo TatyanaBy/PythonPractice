5b0616b6e157b5e55feca8275d9f8fed4c774afd
Tatyana <Alehina8811@gmail.com>
2022-10-18 15:11:12 +0300

    Задача 4 не решена

--- homework2.py
+++ homework2.py
@@ -3,14 +3,15 @@
 # 6782 -> 23
 # 0,56 -> 11
 
-# в случае ввода целого числа
+# работает только с целом числом
 # number = int (input ("Введите число: ")) 
 # sum = 0 
 # while number != 0: 
 #       sum = sum + number % 10 
 #       number = number // 10 
 # print (sum)
-# в случае если вводим вещественное число
+
+# в случае с вещественным 
 # number = (input ("Введите число: ")) 
 # sum = 0 
 # for a in number:
@@ -30,7 +31,7 @@
 # i=1
 # for i in range(n):
 #     if i <=n:
-#        i= i+1
+#        i += 1
 #        result *= i
 #        print(result, end= ' ')       
 
@@ -42,17 +43,35 @@
 
 
 # print(f'Введите n: ')
-n=int(input('Введите n: '))
-d={a: (1+1/a)**a for a in range(1,n+1)}
-print(d)
-values = d.values()
-result = sum(values)
-print(f'Cумма значений равна: {result}')
+# n=int(input('Введите n: '))
+# d={a: (1+1/a)**a for a in range(1,n+1)}
+# print(d)
+# values = d.values()
+# result = sum(values)
+# print(f'Cумма значений равна: {result}')
 
 
 # 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. 
 # Позиции хранятся в файле file.txt в одной строке одно число.
-
+# не решено.
 
 # 5. Реализуйте алгоритм перемешивания списка.
+# 1 вариант:
 
+# list1 = [1,2,3,4,5]
+# for i in range(len(list1)): 
+#     if i < len(list1):
+#        i+=1
+# print(list1)
+# list2 = list(reversed(list1))
+# print(list2)
+
+# 2 вариант:
+# import random
+# n = int(input('Введите n: '))
+# list1 = []
+# for i in range(n):
+#     list1.append(random.randint(-10,100))
+# print(list1)   
+# list2 = list(reversed(list1))
+# print(list2)
