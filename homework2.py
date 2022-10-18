89bf83d16e3a0359302607d781596018c9d31076
Tatyana <Alehina8811@gmail.com>
2022-10-18 15:16:50 +0300

    Все задачи,кроме 4, решены

--- homework2.py
+++ homework2.py
@@ -4,20 +4,20 @@
 # 0,56 -> 11
 
 # работает только с целом числом
-# number = int (input ("Введите число: ")) 
-# sum = 0 
-# while number != 0: 
-#       sum = sum + number % 10 
-#       number = number // 10 
-# print (sum)
+number = int (input ("Введите число: ")) 
+sum = 0 
+while number != 0: 
+      sum = sum + number % 10 
+      number = number // 10 
+print (sum)
 
 # в случае с вещественным 
-# number = (input ("Введите число: ")) 
-# sum = 0 
-# for a in number:
-#     if a.isdigit():
-#         sum += int(a)
-# print(sum)
+number = (input ("Введите число: ")) 
+sum = 0 
+for a in number:
+    if a.isdigit():
+        sum += int(a)
+print(sum)
 
 
 
@@ -26,14 +26,14 @@
 # пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
 
-# n = 4
-# result=1
-# i=1
-# for i in range(n):
-#     if i <=n:
-#        i += 1
-#        result *= i
-#        print(result, end= ' ')       
+n = 4
+result=1
+i=1
+for i in range(n):
+    if i <=n:
+       i += 1
+       result *= i
+       print(result, end= ' ')       
 
 
 # Задача 3. Задайте список из n чисел последовательности (1+1/n)n и выведите на экран их сумму
@@ -42,13 +42,13 @@
 # Сумма 9.06
 
 
-# print(f'Введите n: ')
-# n=int(input('Введите n: '))
-# d={a: (1+1/a)**a for a in range(1,n+1)}
-# print(d)
-# values = d.values()
-# result = sum(values)
-# print(f'Cумма значений равна: {result}')
+print(f'Введите n: ')
+n=int(input('Введите n: '))
+d={a: (1+1/a)**a for a in range(1,n+1)}
+print(d)
+values = d.values()
+result = sum(values)
+print(f'Cумма значений равна: {result}')
 
 
 # Задача 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. 
