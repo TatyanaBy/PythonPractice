1a6e4491076233d149b277f482c16ea59ed64da4
Tatyana <Alehina8811@gmail.com>
2022-11-29 11:10:31 +0300

    interface.py

---
+++ interface.py
@@ -0,0 +1,17 @@
+def enter_float():
+    while True:
+        try:
+            value=float (input())
+            break
+        except:
+            print("Ошибка! Введите число:")
+    return value
+
+def enter_numbers():  #функция ввода чисел с проверкой
+    print('введите два числа: ')
+    a=enter_float()
+    b=enter_float()
+    return (a,b)  #возвращение кортежа
+
+
+
