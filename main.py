0d41494c6a73563eddca106401fea066a985fb51
Tatyana <Alehina8811@gmail.com>
2022-11-29 11:17:02 +0300

    main

---
+++ main.py
@@ -0,0 +1,17 @@
+# import controller as c
+
+# c.button_click()
+
+import interface
+import mult
+import sum
+import div
+import sub
+import logger
+import operators
+a, b = interface.enter_numbers()
+# print(a, b)
+
+print(operators.operator(a,b))
+
+              
