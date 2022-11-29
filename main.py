766ea0f74ffac4030781b3d628be2e6d40b57edd
Tatyana <Alehina8811@gmail.com>
2022-11-29 16:48:03 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import interface
+import logger
+import result
+last_name, first_name, phone_number, comment = interface.get_user()
+print(last_name, first_name, phone_number, comment)
+# print(result.res(last_name, first_name, phone_number, comment))   #лишнее действие? Без него работает, запись идет в лог
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  
+ 
